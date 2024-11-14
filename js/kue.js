c1c4ed9b6243027170314a9159362286753a8fb8
asessment <muhammadaryaputra1190@gmail.com>
2024-11-14 16:41:33 +0700

    up err

--- js/kue.js
+++ js/kue.js
@@ -101,7 +101,7 @@ let navbar = document.querySelector('.navbar');
 
         // Define the opening and closing times in minutes since midnight
         const openingTimeInMinutes = 10 * 60; // 09:00
-        const closingTimeInMinutes = 16 * 60; // 13:00
+        const closingTimeInMinutes = 15 * 60; // 13:00
 
         // Check if the current time is outside of opening hours
         if (currentTimeInMinutes < openingTimeInMinutes || currentTimeInMinutes >= closingTimeInMinutes) {
